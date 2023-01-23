610bd36a2a60c6da027a0d4428c6ae3aa5cd9223
davidarebuwa <davidarebuwa@gmail.com>
2023-01-23 20:07:05 +0000

    updated button component tests

--- components/tests/Button.test.js
+++ components/tests/Button.test.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Button from "../Button";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render, screen, fireEvent } from "@testing-library/react-native";
 
 describe("Button", () => {
     it("should render correctly", () => {
-        const { toJSON } = render(<Button />);
-        expect(toJSON()).toMatchSnapshot();
-    });
-    
-    it("should call onPress when pressed", () => {
-        const onPress = jest.fn();
-        const { getByTestId } = render(<Button onPress={onPress} />);
-        fireEvent.press(getByTestId("button"));
-        expect(onPress).toHaveBeenCalled();
-    });
+        render(<Button />);
+        const title = screen.getByText("Menu");
+        expect(title).toBeTruthy();
+        
     });
 
+    it("should call onPress when clicked", () => {
+        const onPress = jest.fn();
+        render(<Button onPress={onPress} />);
+        const button = screen.getByText("Menu");
+        fireEvent.press(button);
+        expect(onPress).toHaveBeenCalled();
     
+    });
+
+});
+
+
